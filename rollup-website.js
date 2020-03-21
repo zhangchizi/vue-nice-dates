20cff56bb589471c139b20a858567a19622cc36e
zhangchizi <zhangchizi123@foxmail.com>
2020-03-21 17:15:52 +0800

    seperate website config from rollup-server.js

--- rollup-website.js
+++ rollup-website.js
@@ -1,22 +1,20 @@
-import path from 'path'
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
 import vue from 'rollup-plugin-vue'
 import postcss from 'rollup-plugin-postcss'
 import babel from 'rollup-plugin-babel'
 import resolve from '@rollup/plugin-node-resolve'
 import replace from '@rollup/plugin-replace'
 import common from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
 
 export default {
   input: 'website/index.js',
   output: {
     file: 'website/src/main.js',
     format: 'iife',
-    sourcemap: true
+    sourcemap: false
   },
   plugins: [
-    replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+    replace({ 'process.env.NODE_ENV': JSON.stringify('production') }),
     vue(),
     postcss(),
     resolve({
@@ -24,15 +22,6 @@ export default {
     }),
     common(),
     babel({ exclude: ['node_modules/**'] }),
-    serve({
-      open: false,
-      contentBase: path.join(__dirname, 'website', 'src'),
-      host: '0.0.0.0',
-      port: 1234
-    }),
-    livereload({
-      verbose: true,
-      watch: ['website', 'examples', 'src']
-    })
+    terser()
   ]
 }
